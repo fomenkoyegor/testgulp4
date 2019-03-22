3afcaf8802ced6cb4609f2b4562e67ca746aded6
fomenkoyegor <fomenkoyegor@gmail.com>
2019-03-22 17:24:56 +0200

    some js to test

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,3 +1,28 @@
-const random = (min=0,max=1)=> Math.round(Math.random() * (max - min) + min);
-console.log(random(5,60));
-console.log($("body"));
+const log = (data) => console.log(data);
+const links = document.querySelectorAll("header nav ul li a");
+const main = document.querySelector("main");
+const url = "../static/pages/";
+
+const getContent = (page = 'main') => {
+    links.forEach((link, index) => {
+        link.classList.remove("active");
+        if (link.dataset.src == page) {
+            link.classList.add("active");
+        }
+    })
+    main.classList.remove("show");
+    fetch(url + page + ".html")
+        .then(res => res.text())
+        .then(res => {
+            main.innerHTML = '';
+            main.innerHTML = res;
+            main.classList.add("show");
+        })
+}
+
+getContent();
+
+links.forEach((link, index) => link.addEventListener("click", e => {
+    e.preventDefault();
+    getContent(e.target.dataset.src)
+}))
