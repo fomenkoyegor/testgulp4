3e8739512be239611832c1dd6e9f144a95134449
fomenkoyegor <fomenkoyegor@gmail.com>
2019-03-22 13:34:05 +0200

    add autoprefixer and clear plagin

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,39 @@ const sass = require("gulp-sass");
 const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 const { src, dest, task, parallel, watch } = require("gulp");
+const dir = { src: "./app/", dest: "./dist/" }
 
-const htmlSrc = "./app/*.html";
-const htmlDest = "./dist/";
+const htmlSrc = `${dir.src}*.html`;
+const htmlDest = dir.dest;
 const copyHtml = () => src(htmlSrc).pipe(dest(htmlDest));
 task("copyHtml", copyHtml);
 
-const imagesSrc = "./app/assets/images/*";
-const imagesDest = "./dist/assets/images/";
+const imagesSrc = `${dir.src}assets/images/*`;
+const imagesDest = `${dir.dest}assets/images/`;
 const imageMin = () => src(imagesSrc).pipe(imagemin()).pipe(dest(imagesDest));
 task("imageMin", imageMin);
 
-const scriptSrc = "./app/assets/js/*.js";
-const scriptDest = "./dist/assets/js/";
+const scriptSrc = `${dir.src}assets/js/*.js`;
+const scriptDest = `${dir.dest}assets/js/`;
 const scriptName = "main.js";
 const scripts = () => src(scriptSrc).pipe(concat(scriptName)).pipe(uglify()).pipe(dest(scriptDest));
 task("scripts", scripts);
 
-const styleSrc = "./app/assets/styles/**/*.scss";
-const styleDest = "./dist/assets/styles/";
-const style = () => src(styleSrc).pipe(sass().on("error", sass.logError)).pipe(dest(styleDest));
+const styleSrc = `${dir.src}assets/styles/**/*.scss`;
+const styleDest = `${dir.dest}assets/styles/`;
+const styleName = "main.css";
+const style = () => src(styleSrc)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat(styleName))
+    .pipe(autoprefixer({
+        browsers: ['last 5 versions', '> 1%', 'ie 8', 'ie 7'],
+        cascade: false
+    }))
+    .pipe(cleanCSS({level: 2}))
+    .pipe(dest(styleDest));
 task("style", style);
 
 const watchFiles = () => {
