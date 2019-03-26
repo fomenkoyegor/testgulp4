12b861dfd02c5d447efd0e8ae5b6efa10f66271e
fomenkoyegor <fomenkoyegor@gmail.com>
2019-03-26 15:54:43 +0200

    add polifil async

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,54 +1,84 @@
-import from from 'core-js-pure/features/array/from';
+"use strict";
+// import from from 'core-js-pure/features/array/from';
 // import flat from 'core-js-pure/features/array/flat';
 // import Set from 'core-js-pure/features/set';
-// import Promise from 'core-js-pure/features/promise';
+import  Promise from 'core-js-pure/features/promise';
+import { Axios } from 'axios';
 import random from './random';
-
+import EventObserver from './event-observer';
+import { async, resolve, reject } from 'q';
+const observer = new EventObserver();
+const axios = new Axios();
 const log = (data) => console.log(data);
 const links = document.querySelectorAll("header nav ul li a");
 
 const main = document.querySelector("main");
 const url = "../static/pages/";
-                                      
-
-
+const getPage = async (url,page) =>{
+    const res = await axios.get(url + page + ".html");
+    return res.data;
+}                                     
 
 const getContent = (page = 'main') => {
     links.forEach((link, index) => {
+        observer.subscribe(data => {
+            console.log("click on link " + data.link)
+        })
         link.classList.remove("active");
         if (link.dataset.src == page) {
             link.classList.add("active");
         }
     })
     main.classList.remove("show");
-    fetch(url + page + ".html")
-        .then(res => res.text())
-        .then(res => {
-            main.innerHTML = '';
-            main.innerHTML = res;
-            main.classList.add("show");
-        })
+    // fetch(url + page + ".html")
+    //     .then(res => res.text())
+    //     .then(res => {
+    //         main.innerHTML = '';
+    //         main.innerHTML = res;
+    //         main.classList.add("show");
+    //     })
+    // axios.get(url + page + ".html")
+    //     .then(res => {
+    //         main.innerHTML = '';
+    //         main.innerHTML = res.data;
+    //         main.classList.add("show");
+    //     });
+    getPage(url,page).then(data=>{
+        main.innerHTML = '';
+        main.innerHTML = data;
+        main.classList.add("show"); 
+    })  
+   
+
 }
 
-getContent();     
-         
+
+(new Promise((resolve,reject)=>setTimeout(resolve,500)))
+    .then(res=>console.log("promise"))
+
+
+
+
+getContent();
+
 links.forEach((link, index) => link.addEventListener("click", e => {
     e.preventDefault();
-    getContent(e.target.dataset.src)
+    getContent(e.target.dataset.src);
+    observer.broadcast({ link: e.target.dataset.src });
 }))
 
 
 const items = document.querySelectorAll(".item");
 // const random = (min,max)=>Math.round(Math.random() * (max - min) + min);
-const setActiveItem = ()=>{
-    items.forEach((item,index)=>{
-     item.classList.remove('show');
-     if(index==random(0,items.length-1)){
-       item.classList.add('show');    
-     }
+const setActiveItem = () => {
+    items.forEach((item, index) => {
+        item.classList.remove('show');
+        if (index == random(0, items.length - 1)) {
+            item.classList.add('show');
+        }
     })
-  }
-  setInterval(()=>setActiveItem(),1000);
+}
+setInterval(() => setActiveItem(), 1000);
 
 
 //   class State {
@@ -72,12 +102,12 @@ const setActiveItem = ()=>{
 //   console.log(meneger.state());
 
 
-class A{ 
-    getA(){      
-        console.log( "B" )
+class A {
+    getA() {
+        console.log("B")
     }
 }
 
 const a = new A();
 a.getA();
-log(random(1,2))
+log(random(1, 2))
